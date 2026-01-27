fa907e325d8e86d5b693920ae862812fd16e2f68
Alizhan Toibazar <phamiliaimia@gmail.com>
2026-01-27 22:34:25 +0500

    test runner

--- js/script.js
+++ js/script.js
@@ -969,10 +969,8 @@ function addQuestion(insertAfterElement = null) {
         </div>
         <div class="form-group">
             <label data-translate="quiz.questionText">Question Text</label>
-            <div class="input-with-image">
-                <div class="rich-text-input question-text" contenteditable="true" data-translate-placeholder="quiz.questionPlaceholder" data-placeholder="Enter your question here..."></div>
-                <button class="image-upload-btn" onclick="handleImageUpload(this, 'question')" title="Add image"><svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="black"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg></button>
-            </div>
+            <div class="rich-text-input question-text" contenteditable="true" data-translate-placeholder="quiz.questionPlaceholder" data-placeholder="Enter your question here..."></div>
+            <button class="image-upload-btn" onclick="handleImageUpload(this, 'question')" title="Add image"><svg xmlns="http://www.w3.org/2000/svg" height="16px" viewBox="0 -960 960 960" width="16px" fill="black"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm40-80h480L570-480 450-320l-90-120-120 160Zm-40 80v-560 560Z"/></svg></button>
             <div class="image-preview-container" id="question-preview-${questionCount}"></div>
         </div>
         <div class="question-content"></div>
